cc99e1b7c847b1b2a0a10427793d1e7ce7aa1f21
Mahdi-Abunemer <163647199+Mahdi-Abunemer@users.noreply.github.com>
2026-05-03 21:20:19 +0700

    Refactored IConsole interface file , the defects were  bad separation into files.

--- PersonalFinanceCli/Presentation/Rendering/ConsoleAbstractions.cs
+++ PersonalFinanceCli/Presentation/Rendering/ConsoleAbstractions.cs
@@ -12,28 +12,3 @@ public interface IConsole
 
     TextWriter Out { get; }
 }
-
-public sealed class SystemConsole : IConsole
-{
-    public string? ReadLine()
-    {
-        return Console.ReadLine();
-    }
-
-    public void Write(string text)
-    {
-        Console.Write(text);
-    }
-
-    public void WriteLine(string text)
-    {
-        Console.WriteLine(text);
-    }
-
-    public void WriteLine()
-    {
-        Console.WriteLine();
-    }
-
-    public TextWriter Out => Console.Out;
-}
