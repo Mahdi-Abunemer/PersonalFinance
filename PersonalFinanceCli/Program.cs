c5f8932c2db76bed113a0ca160948d8f0eff849e
Mahdi-Abunemer <163647199+Mahdi-Abunemer@users.noreply.github.com>
2026-05-03 23:45:38 +0700

    Refactored Program file , the defects were bad naming, comments and formatting.

--- PersonalFinanceCli/Program.cs
+++ PersonalFinanceCli/Program.cs
@@ -12,32 +12,44 @@ public static class Program
 {
     public static int Main(string[] args)
     {
-        // main method starts when app starts, usually
         var console = new SystemConsole();
-        // this is a file path and we use json because json is text
         var dataPath = Path.Combine(Directory.GetCurrentDirectory(), "data.json");
 
-        // repositories are here to keep repository things
-        var store = new JsonDataStore(dataPath);
-        var cardRepository = new JsonCardRepository(store);
-        var transactionRepository = new JsonTransactionRepository(store);
-        var limitRepository = new JsonLimitRepository(store);
-        var onboardingStateRepository = new JsonOnboardingStateRepository(store);
+        var dataStore = new JsonDataStore(dataPath);
+        var cardRepository = new JsonCardRepository(dataStore);
+        var transactionRepository = new JsonTransactionRepository(dataStore);
+        var limitRepository = new JsonLimitRepository(dataStore);
+        var onboardingStateRepository = new JsonOnboardingStateRepository(dataStore);
         var clock = new SystemClock();
 
         var parser = new CommandParser();
         var addCardHandler = new AddCardHandler(cardRepository);
         var setDefaultCardHandler = new SetDefaultCardHandler(cardRepository);
-        var addTransactionHandler = new AddTransactionHandler(transactionRepository, cardRepository, clock);
+        var addTransactionHandler = new AddTransactionHandler(
+            transactionRepository,
+            cardRepository,
+            clock);
         var addIncomeHandler = new AddIncomeHandler(addTransactionHandler);
-        var addExpenseHandler = new AddExpenseHandler(transactionRepository, cardRepository, clock);
-        var setDailyLimitHandler = new SetDailyLimitHandler(limitRepository, cardRepository, clock);
-        var dailyReportService = new DailyReportService(cardRepository, transactionRepository, limitRepository);
+        var addExpenseHandler = new AddExpenseHandler(
+            transactionRepository,
+            cardRepository,
+            clock);
+        var setDailyLimitHandler = new SetDailyLimitHandler(
+            limitRepository,
+            cardRepository,
+            clock);
+        var dailyReportService = new DailyReportService(
+            cardRepository,
+            transactionRepository,
+            limitRepository);
         var cushionService = new CushionService(cardRepository);
-        var reportPrinter = new ReportPrinter(console.Out, cardRepository, transactionRepository, limitRepository);
+        var reportPrinter = new ReportPrinter(
+            console.Out,
+            cardRepository,
+            transactionRepository,
+            limitRepository);
 
-        // UI is created before we use it later below
-        var ui = new ConsoleUi(
+        var consoleUi = new ConsoleUi(
             parser,
             addCardHandler,
             setDefaultCardHandler,
@@ -54,15 +66,12 @@ public static class Program
             console,
             cushionService);
 
-        // if there are args then it is non-interactive interactive mode
         if (args.Length > 0)
         {
-            return ui.Execute(args);
+            return consoleUi.Execute(args);
         }
 
-        // this loop exits when user exits, or not
-        ui.RunInteractiveLoop();
-        // zero means success except when it does not
+        consoleUi.RunInteractiveLoop();
         return 0;
     }
 }
